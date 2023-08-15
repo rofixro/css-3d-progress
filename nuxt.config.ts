b64cab0e4aa4e4c10f70d0587b15187f7279f161
Niyaco <frankiewb9527@gmail.com>
2023-08-15 16:15:25 +0800

    chore: add unocss vite-plugin-cesium

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,12 @@
+import cesium from 'vite-plugin-cesium';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  modules: ['@unocss/nuxt'],
   devtools: { enabled: true },
-  runtimeConfig: {
-    public: { cesiumToken: '' }
-  }
+  unocss: { attributify: true },
+  vite: { plugins: [cesium()] },
+  css: ['@unocss/reset/tailwind.css'],
+  runtimeConfig: { public: { cesiumToken: '' } }
 })
