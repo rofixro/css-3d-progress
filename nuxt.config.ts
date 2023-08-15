1fadf5888a905c4884ba8da2c422589c44cafd1a
Niyaco <frankiewb9527@gmail.com>
2023-08-15 14:28:02 +0800

    chore: add env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: { cesiumToken: '' }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Ion, Viewer } from 'cesium';
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
+onMounted(() => {
+    const runtimeConfig = useRuntimeConfig()
+    const viewer = new Viewer('cesiumContainer');
+})
+</script>
+
+<template>
+    <main id="cesiumContainer"></main>
+</template>
