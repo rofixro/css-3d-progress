9b4343705e309c8fd094b21bc8094da68d7fc602
Niyaco <frankiewb9527@gmail.com>
2023-08-16 11:56:30 +0800

    feat: hide cesium controls

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,14 @@ Ion.defaultAccessToken = runtimeConfig.public.cesiumToken;
 onMounted(async () => {
     const viewer = new Viewer('cesiumContainer', {
         infoBox: false,
+        geocoder: false,
+        timeline: false,
+        animation: false,
+        homeButton: false,
+        baseLayerPicker: false,
+        sceneModePicker: false,
+        fullscreenButton: false,
+        navigationHelpButton: false,
         terrain: Terrain.fromWorldTerrain()
     });
 
