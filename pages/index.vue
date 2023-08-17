624077d433ce9bd64406856e44e31c958c135406
Niyaco <frankiewb9527@gmail.com>
2023-08-17 16:12:36 +0800

    feat: use default token

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import "cesium/Build/Cesium/Widgets/widgets.css";
-import { Ion, Viewer, Terrain, Cesium3DTileset } from 'cesium';
+import { Ion, Viewer, Terrain, Cesium3DTileset, IonResource } from 'cesium';
 
-const runtimeConfig = useRuntimeConfig()
-Ion.defaultAccessToken = runtimeConfig.public.cesiumToken;
+// const runtimeConfig = useRuntimeConfig()
+// Ion.defaultAccessToken = runtimeConfig.public.cesiumToken;
 
 onMounted(async () => {
     const viewer = new Viewer('cesiumContainer', {
@@ -20,7 +20,8 @@ onMounted(async () => {
     });
 
     try {
-        const tileset = await Cesium3DTileset.fromUrl('https://assets.ion.cesium.com/40866/tileset.json?v=2');
+        const resource = await IonResource.fromAssetId(40866);
+        const tileset = await Cesium3DTileset.fromUrl(resource);
 
         viewer.scene.primitives.add(tileset);
         viewer.zoomTo(tileset);
